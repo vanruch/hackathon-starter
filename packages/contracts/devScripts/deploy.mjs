ae02d095990b23cfdf564a8108fe117c29cbeef8
nezouse <stankiewicz1998@gmail.com>
2023-02-17 00:27:38 +0100

    Add contracts package

---
+++ packages/contracts/devScripts/deploy.mjs
@@ -0,0 +1,43 @@
+import "zx/globals";
+import { spinner } from "zx/experimental";
+import fs from "fs/promises";
+
+const rpcs = {
+  31337: "http://127.0.0.1:8545",
+};
+
+$`anvil`;
+
+await spinner("Waiting for anvil to be ready", () => sleep(1000));
+
+const chainId = argv.chainId ?? 31337;
+const scriptName = (argv.scriptName ?? "Counter") + ".s.sol";
+const scriptPath = path.join(__dirname, `../script/${scriptName}`);
+
+await $`forge script ${scriptPath} --rpc-url ${rpcs[chainId]} --broadcast`;
+
+const transactionsPath = path.join(
+  __dirname,
+  `../broadcast/${scriptName}/${chainId}/run-latest.json`
+);
+
+const { transactions } = JSON.parse(
+  await fs.readFile(transactionsPath, "utf-8")
+);
+
+const deployments = Object.fromEntries(
+  transactions
+    .filter((tx) => tx.transactionType === "CREATE")
+    .map((tx) => [
+      tx.contractName,
+      {
+        txHash: tx.hash,
+        address: tx.contractAddress,
+      },
+    ])
+);
+
+await fs.writeFile(
+  path.join(__dirname, `../deployments/${chainId}.json`),
+  JSON.stringify(deployments, null, 2)
+);
--- packages/contracts/script/Counter.s.sol
+++ packages/contracts/script/Counter.s.sol
@@ -1,12 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.18;
 
 import "forge-std/Script.sol";
+import "../src/Counter.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        uint256 privateKey = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+        vm.startBroadcast(privateKey);
+
+        Counter counter = new Counter();
+        counter.setNumber(10);
+
+        vm.stopBroadcast();
     }
 }
